c650e20131133955f5633461c5bb398900ff7b6a
xEnsy <91109506+xEnsy@users.noreply.github.com>
2023-08-14 13:52:10 +0300

    Create cummback.py

---
+++ handlers/cummback.py
@@ -0,0 +1,19 @@
+from config import bot
+from aiogram import types, Dispatcher
+from database.commands import Database
+from aiogram.types import ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+async def gay_list(call: types.CallbackQuery):
+    users = Database().sql_user()
+    print(users)
+    print(str(users))
+    doto = []
+    for i in users:
+        doto.append(str(i))
+    doto = ''.join(doto)
+    await call.message.reply(f"{doto}")
+
+
+def register_handlers_cummback(dp: Dispatcher):
+    dp.register_callback_query_handler(gay_list, lambda call: call.data == "gay_list")
