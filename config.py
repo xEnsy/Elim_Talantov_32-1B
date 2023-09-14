78ca1e573788743c00349a72b5e4cedadc51be40
xEnsy <91109506+xEnsy@users.noreply.github.com>
2023-09-14 20:46:29 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import decouple
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-storage = MemoryStorage()
-TOKEN = decouple.config("TOKEN")
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot, storage=storage)
