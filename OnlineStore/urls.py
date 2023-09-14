23abe59a0afabd1decca129686ae393da6a8cdb1
xEnsy <91109506+xEnsy@users.noreply.github.com>
2023-09-14 20:48:55 +0300

    Create urls.py

---
+++ OnlineStore/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for OnlineStore project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from posts import views as posts_vies
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('hello', posts_vies.hello),
+    path('now_date', posts_vies.now_date),
+    path('goodbye', posts_vies.goodbye),
+    path('bruh69', posts_vies.bruh69)
+]
